cea22e2ca46a8fcf607a3f344fac966239727ccc
Anna <rozhdestvenskaya06@gmail.com>
2025-11-20 00:13:50 +0300

    Написала словарь предметов

---
+++ lab4.py
@@ -0,0 +1,14 @@
+base_rate = 15
+stuffdict = {'rifle':("r",3,25), 
+             'pistol':("p",2,15), 
+             'ammo':("a",2,15), 
+             'medkit':("m",2,20), 
+             'inhaler':("i",1,5), 
+             'knife':("k",1,15), 
+             'axe':("a",3,20),
+             'talisman':("t",1,25),
+             'flask':("f",1,15),
+             'antidot':("a",1,10), 
+             'supplies':("s",2,20),
+             'crossbow':("c",2,20)
+            }
